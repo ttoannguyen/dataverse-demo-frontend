16cbaefa1e206b2aa4a146d4904947621b0f929e
Kin-afk-ops <linhqwertyuiop2@gmail.com>
2025-04-13 22:22:17 +0700

    update datasetDetail

--- src/components/dataverse/DatasetDetail.jsx
+++ src/components/dataverse/DatasetDetail.jsx
@@ -72,331 +72,161 @@ const DatasetDetail = () => {
   console.log(dataset);
 
   return (
-    <>
-      <div className="p-4">
-        <div>
-          <Link className="">
-            Lowy Institute Being Chinese in Australia Dataverse
-          </Link>
-          <p>(Lowy Institute)</p>
-        </div>
+    <div className="p-4">
+      <div className="text-base pt-[28px] pb-[28px] px-0">
+        <Link className="block text-hover-underline-blue  mt-[5px] text-[18px] mb-[10px]">
+          {metadata.topic}
+        </Link>
+        <p style={{ color: "#666666" }}>{"(" + metadata.organization + ")"}</p>
+      </div>
 
-        <div>Lowy Institute Being Chinese in Australia Dataverse</div>
+      <div className="">
+        <span className="text-hover-underline-blue">CTU Dataverse</span> &gt;{" "}
+        <span className="text-hover-underline-blue">{metadata.topic}</span>
+      </div>
 
-        <h1>{metadata.title}</h1>
-        <p>Version 1.0</p>
-        <div className="grid grid-cols-[75%_25%] gap-4">
-          <div class=" p-4  text-sm">
-            <div className="p-4 custom-light-blue grid grid-cols-[15%_85%] ">
-              <div className="w-full text-xl ">
-                <img src={defaultFile} alt="" />
-              </div>
+      <h1 className="text-[36px] leading-[1.1] font-bold mt-4 mb-0">
+        {metadata.title}
+      </h1>
+      <span className="label-default">{metadata.license}</span>
+      <div className="grid grid-cols-[75%_25%] gap-4">
+        <div className=" pt-4 pm-4 text-sm">
+          <div className="p-4 custom-light-blue grid grid-cols-[15%_85%] ">
+            <div className="w-full text-xl ">
+              <img src={defaultFile} alt="" />
+            </div>
+            <div className="pl-4">
               <div>
-                <div>
-                  Hsu, Jennifer, 2025, "Being Chinese in Australia 2021
-                  release",
-                  <Link to={"https://doi.org/10.26193/ZPBVNW"}>
-                    {" "}
-                    https://doi.org/10.26193/ZPBVNW
-                  </Link>
-                  , ADA Dataverse, V1
-                </div>
-
-                <div className="grid grid-cols-[30%_70%] ">
-                  <div class=" p-4">
-                    <div className="relative inline-block text-left">
-                      <button
-                        onClick={toggleDropdown}
-                        className="cursor-pointer flex items-center text-hover-underline-blue "
-                      >
-                        <span> Cite Dataset</span>
-                      </button>
-
-                      {isOpen && (
-                        <div className="absolute z-10 mt-2 w-44 bg-white border border-gray-200 rounded shadow-lg">
-                          <a
-                            href="#"
-                            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                          >
-                            Option 1
-                          </a>
-                          <a
-                            href="#"
-                            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                          >
-                            Option 2
-                          </a>
-                          <a
-                            href="#"
-                            className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                          >
-                            Option 3
-                          </a>
-                        </div>
-                      )}
-                    </div>
-                  </div>
-                  <div class="text-left p-4">
-                    Learn about Data Citation Standards.
-                  </div>
-                </div>
-              </div>
-            </div>
-
-            <div className="mt-8">
-              <div className=" grid grid-cols-[25%_75%] gap-4">
-                <div className="">Description</div>
-                <div>
-                  The Australian Survey of Social Attitudes (AuSSA) is
-                  Australia’s main source of data for the scientific study of
-                  the social attitudes, beliefs and opinions of Australians, how
-                  they change over time, and how they compare with other
-                  societies. The survey is used to help researchers better
-                  understand how Australians think and feel about their lives.
-                  It produces important information about the changing views and
-                  attitudes of Australians as we move through the 21st century.
-                  Similar surveys are run in other countries, so data from the
-                  AuSSA also allows us to compare Australia with countries all
-                  over the world. The aims of the survey are to discover: the
-                  range of Australians’ views on topics that are important to
-                  all of us; how these views differ for people in different
-                  circumstances; how they have changed over the past quarter
-                  century; and how they compare with people in other countries.
-                  AuSSA is also the Australian component of the International
-                  Social Survey Project (ISSP). The ISSP is a cross-national
-                  collaboration on surveys covering important topics. Each year,
-                  survey researchers in some 40 countries each do a national
-                  survey using the same questions. The ISSP focuses on a special
-                  topic each year, repeating that topic from time to time. The
-                  topic for the 2021 survey is "Health and Healthcare". This is
-                  the second time this has been the topic of the survey, having
-                  previously been the theme for the survey in 2011. The data
-                  from questions in sections B, C, D, E, F and G are embargoed
-                  until 1 January 2025.
-                </div>
-              </div>
-
-              <div className=" mt-4 grid grid-cols-[25%_75%] gap-4">
-                <div className="">Subject</div>
-                <div>Social Sciences</div>
-              </div>
-
-              <div className="mt-4 grid grid-cols-[25%_75%] gap-4">
-                <div className="">Related Publication </div>
-                <div>
-                  Publications using AuSSA Data or citing AuSSA url:
-                  https://www.acspri.org.au/aussa/publications
-                </div>
-              </div>
-
-              <div className="mt-4 grid grid-cols-[25%_75%] gap-4">
-                <div className="">License/Data Use Agreement</div>
-                <div>Custom Dataset Terms</div>
-              </div>
-            </div>
-          </div>
-          <div class="bg-blue-100 p-4">
-            <div className="relative inline-block text-left">
-              <button
-                onClick={() => setAccessIsOpen(!accessIsOpen)}
-                className="cursor-pointer"
-              >
-                Menu
-              </button>
-
-              {accessIsOpen && (
-                <div className="absolute z-10 mt-2 w-44 bg-white border border-gray-200 rounded shadow-lg">
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                  >
-                    Option 1
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                  >
-                    Option 2
-                  </a>
-                  <a
-                    href="#"
-                    className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
-                  >
-                    Option 3
-                  </a>
-                </div>
-              )}
-            </div>
-
-            <div className="grid grid-cols-2">
-              <div className="bg-blue-100 flex justify-center items-center">
-                Contact Owner
-              </div>
-              <div className="bg-red-100 flex justify-center items-center">
-                Share
-              </div>
-            </div>
-          </div>
-        </div>
-      </div>
-
-      <div className="p-4">
-        <button
-          onClick={() => navigate("/datasets")}
-          className="mb-4 text-green-600 hover:underline"
-        >
-          Back to Dataset List
-        </button>
-
-        <h2 className="text-2xl font-semibold text-green-800 mb-4">
-          {metadata.title}
-        </h2>
-
-        <p className="text-gray-700 mb-4">{metadata.description}</p>
-
-        <div className="bg-gray-100 p-4 rounded-lg mb-4">
-          <h3 className="text-lg font-semibold mb-2">Metadata</h3>
-          <p>
-            <strong>Type:</strong> {type}
-          </p>
-          <p>
-            <strong>Organization:</strong> {metadata.organization}
-          </p>
-          <p>
-            <strong>Country:</strong> {metadata.country}
-          </p>
-          <p>
-            <strong>Language:</strong> {metadata.language}
-          </p>
-          <p>
-            <strong>License:</strong> {metadata.license}
-          </p>
-          <p>
-            <strong>Total Records:</strong> {metadata.total_records}
-          </p>
-          <p>
-            <strong>Keywords:</strong> {metadata.keywords.join(", ")}
-          </p>
-        </div>
-
-        <div>
-          <h3 className="text-lg font-semibold mb-2">Data Records</h3>
-          {data.length > 0 ? (
-            <div className="space-y-4">
-              {data.map((record, index) => (
-                <div
-                  key={index}
-                  className="border p-3 rounded-lg bg-white shadow-sm"
+                Hsu, Jennifer, 2025, "Being Chinese in Australia 2021 release",
+                <Link
+                  to={"https://doi.org/10.26193/ZPBVNW"}
+                  className="text-hover-link-blue"
                 >
-                  {type === "population" && (
-                    <>
-                      <p>
-                        <strong>Individual ID:</strong> {record.individual_id}
+                  {" "}
+                  https://doi.org/10.26193/ZPBVNW
+                </Link>
+                , ADA Dataverse, V1
+              </div>
+
+              <div className="grid grid-cols-[30%_70%] ">
+                <div className=" pt-4">
+                  <div className="relative inline-block text-left">
+                    <button
+                      onClick={toggleDropdown}
+                      className="cursor-pointer flex items-center text-hover-underline-blue "
+                    >
+                      <p className="relative">
+                        {" "}
+                        Cite Dataset
+                        <i className="fa-solid fa-sort-down absolute left-[105%]"></i>
                       </p>
-                      <p>
-                        <strong>District:</strong> {record.district}
-                      </p>
-                      <p>
-                        <strong>Age:</strong> {record.age}
-                      </p>
-                      <p>
-                        <strong>Gender:</strong> {record.gender}
-                      </p>
-                      <p>
-                        <strong>Occupation:</strong> {record.occupation}
-                      </p>
-                      <p>
-                        <strong>Household Size:</strong> {record.household_size}
-                      </p>
-                      <p>
-                        <strong>Income (VND/month):</strong>{" "}
-                        {record.income_vnd_per_month.toLocaleString()}
-                      </p>
-                    </>
-                  )}
-                  {type === "weather" && (
-                    <>
-                      <p>
-                        <strong>Station ID:</strong> {record.station_id}
-                      </p>
-                      <p>
-                        <strong>Location:</strong> {record.location}
-                      </p>
-                      <p>
-                        <strong>Coordinates:</strong> Lat{" "}
-                        {record.coordinates.latitude}, Long{" "}
-                        {record.coordinates.longitude}
-                      </p>
-                      <h4 className="font-medium mt-2">Measurements:</h4>
-                      {record.measurements.map((measurement, idx) => (
-                        <div key={idx} className="ml-4">
-                          <p>
-                            <strong>Date:</strong> {measurement.date}
-                          </p>
-                          <p>
-                            <strong>Temperature (°C):</strong>{" "}
-                            {measurement.temperature_celsius}
-                          </p>
-                          <p>
-                            <strong>Humidity (%):</strong>{" "}
-                            {measurement.humidity_percent}
-                          </p>
-                          <p>
-                            <strong>Rainfall (mm):</strong>{" "}
-                            {measurement.rainfall_mm}
-                          </p>
-                        </div>
-                      ))}
-                    </>
-                  )}
-                  {type === "agriculture" && (
-                    <>
-                      <p>
-                        <strong>Farmer ID:</strong> {record.farmer_id}
-                      </p>
-                      <p>
-                        <strong>Name:</strong> {record.name}
-                      </p>
-                      <p>
-                        <strong>Location:</strong> {record.location}
-                      </p>
-                      <p>
-                        <strong>Farm Size (hectares):</strong>{" "}
-                        {record.farm_size_hectares}
-                      </p>
-                      <h4 className="font-medium mt-2">Crops:</h4>
-                      {record.crops.map((crop, idx) => (
-                        <div key={idx} className="ml-4">
-                          <p>
-                            <strong>Crop Type:</strong> {crop.crop_type}
-                          </p>
-                          <p>
-                            <strong>Variety:</strong> {crop.variety}
-                          </p>
-                          <p>
-                            <strong>Season:</strong> {crop.season}
-                          </p>
-                          <p>
-                            <strong>Yield (tons/hectare):</strong>{" "}
-                            {crop.yield_tons_per_hectare}
-                          </p>
-                          <p>
-                            <strong>Irrigation Method:</strong>{" "}
-                            {crop.irrigation_method}
-                          </p>
-                        </div>
-                      ))}
-                    </>
-                  )}
+                    </button>
+
+                    {isOpen && (
+                      <div className="absolute z-10 mt-2 w-44 bg-white rounded shadow-lg border border-[#ccc]">
+                        <a
+                          href="#"
+                          className="block px-4 py-2 text-gray-800 underline hover:bg-gray-100 hover:no-underline "
+                        >
+                          {metadata.format}
+                        </a>
+                        {/* <a
+                            href="#"
+                            className="block px-4 py-2 text-gray-800 underline hover:bg-gray-100 hover:no-underline "
+                          >
+                            RIS
+                          </a>
+                          <a
+                            href="#"
+                            className="block px-4 py-2 text-gray-800 underline hover:bg-gray-100 hover:no-underline "
+                          >
+                            Bib TeX
+                          </a> */}
+                      </div>
+                    )}
+                  </div>
                 </div>
-              ))}
+                <div className="text-left pt-4">
+                  Learn about{" "}
+                  <Link to={"/"} className="text-hover-link-blue">
+                    Data Citation Standards
+                  </Link>
+                  .
+                </div>
+              </div>
             </div>
-          ) : (
-            <p>No data records available.</p>
-          )}
+          </div>
+
+          <div className="mt-8 pl-4 pr-4">
+            <div className=" grid grid-cols-[15%_85%] gap-4">
+              <div className="font-bold">Description</div>
+              <div>{metadata.description}</div>
+            </div>
+
+            <div className=" mt-4 grid grid-cols-[15%_85%] gap-4">
+              <div className="font-bold">Subject</div>
+              <div>{type}</div>
+            </div>
+
+            <div className="mt-4 grid grid-cols-[15%_85%] gap-4">
+              <div className="font-bold">Related Publication </div>
+              <div>haha</div>
+            </div>
+
+            <div className="mt-4 grid grid-cols-[15%_85%] gap-4">
+              <div className="font-bold">License/Data Use Agreement</div>
+              <div>{metadata.license}</div>
+            </div>
+          </div>
+        </div>
+        <div className="pt-4 pm-4">
+          <div className="relative inline-block text-left w-full">
+            <button
+              onClick={() => setAccessIsOpen(!accessIsOpen)}
+              className="cursor-pointer flex justify-center items-center w-full p-2 primary-btn"
+            >
+              <p className="relative h-full">
+                Access Dataset
+                <i className="fa-solid fa-sort-down absolute left-[105%]"></i>
+              </p>
+            </button>
+
+            {accessIsOpen && (
+              <div className="absolute z-10 mt-1 w-[90%] left-[10%] bg-white border border-[#ccc] rounded shadow-lg p-4">
+                <p className="pl-4 text-[13px]">
+                  Download Options
+                  <i className="ml-2 fa-solid fa-download"></i>
+                </p>
+
+                <a
+                  href="#"
+                  className="block px-4 py-2 text-gray-800 hover:bg-gray-100"
+                >
+                  Download ZIP (12.2MB)
+                </a>
+              </div>
+            )}
+          </div>
+
+          <div className="grid grid-cols-2 mb-[14px]">
+            <button className="default-btn  flex justify-center items-center cursor-pointer ">
+              Contact Owner
+            </button>
+            <button className="default-btn  bg-red-100 flex justify-center items-center cursor-pointer ">
+              Share
+            </button>
+          </div>
+
+          <div className="text-sm">
+            <div> Dataset Metrics </div>
+            <div className="p-4 border-l border-r border-b border-gray-300">
+              {" "}
+              13 Downloads{" "}
+            </div>
+          </div>
         </div>
       </div>
-    </>
+    </div>
   );
 };
 
